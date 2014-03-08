bb42bdf2719429bddfbf548e251cbf88d03d5775
James Cooper <jamespcooper@gmail.com>
2014-03-08 07:29:30 -0800

    removed Internal.hs - moved stuff to parent Week1.hs

--- src/Cis194/Hw/Week1.hs
+++ src/Cis194/Hw/Week1.hs
@@ -1,3 +1,30 @@
 module Cis194.Hw.Week1 where
 
-import Cis194.Hw.Week1.Internal
+-------------
+-- Ex 1-4  --
+-------------
+
+toDigits :: Integer -> [Integer]
+toDigits x = [x]
+
+toDigitsRev :: Integer -> [Integer]
+toDigitsRev x = [x]
+
+doubleEveryOther :: [Integer] -> [Integer]
+doubleEveryOther xs = xs
+
+sumDigits :: [Integer] -> Integer
+sumDigits xs = 0
+
+validate :: Integer -> Bool
+validate x = False
+
+---------------------
+-- Towers of Hanoi --
+---------------------
+
+type Peg = String
+type Move = (Peg, Peg)
+
+hanoi :: Integer -> Peg -> Peg -> Peg -> [Move]
+hanoi x p1 p2 p3 = []
--- test/Cis194/Hw/Week1Spec.hs
+++ test/Cis194/Hw/Week1Spec.hs
@@ -1,12 +1,16 @@
 module Cis194.Hw.Week1Spec (main, spec) where
 
 import Test.Hspec
+import Cis194.Hw.Week1
 
 main :: IO ()
 main = hspec spec
 
 spec :: Spec
 spec = do
-  describe "someFunction" $ do
-    it "should work fine" $ do
-      True `shouldBe` False
+  describe "toDigits" $ do
+    it "should split digits of integer into a list" $ do
+      toDigits 1234 `shouldBe` [1,2,3,4]
+    it "should return an empty list for zero" $ do
+    	toDigits 0 `shouldBe` []
+
