d6fc25ca30acd07f75d1532e27601f93f4417c10
James Cooper <jamespcooper@gmail.com>
2014-03-18 23:59:20 -0400

    Implementation of weeks 1-4

--- src/Cis194/Hw/LogAnalysis.hs
+++ src/Cis194/Hw/LogAnalysis.hs
@@ -3,25 +3,77 @@ module Cis194.Hw.LogAnalysis where
 
 -- in ghci, you may need to specify an additional include path:
 -- Prelude> :set -isrc/Cis194/Hw
-import  Cis194.Hw.Log
+import Cis194.Hw.Log
 
+-- TODO: need to understand error handling. this implementation will blow up
+-- if the error severity or timestamp is not an integer (read throws an exception)
+-- sample.log and error.log are well formed, so this isn't an issue for parsing those files..
+--
+-- It's also unclear what the expected output is for messages without timestamps
+-- e.g.:   I Foo
+--
 parseMessage :: String -> LogMessage
-parseMessage s = Unknown s
+parseMessage s = case mtype of
+    "I" -> LogMessage Info time msg
+    "W" -> LogMessage Warning time msg
+    "E" -> LogMessage (Error sev) time msg
+    _   -> Unknown s
+  where xs          = words s
+        (mtype,xs1) = if length xs == 0 then ("",xs) else (head xs, tail xs)
+        (sev,xs2)   = if mtype == "E" && length xs1 > 1 then (read (head xs1), (tail xs1)) else (-1, xs1)
+        (time,xs3)  = if length xs2 > 1 then (read (head xs2), (tail xs2)) else (-1, xs2)
+        msg         = unwords xs3
 
 parse :: String -> [LogMessage]
-parse _ = []
+parse s = map parseMessage (lines s)
 
 insert :: LogMessage -> MessageTree -> MessageTree
-insert _ t = t
+insert (Unknown _) t                = t
+insert m@(LogMessage _ newtime _) t = case t of
+  Leaf              -> Node Leaf m Leaf
+  Node left n right -> case n of
+    (Unknown _)           -> Node Leaf m Leaf
+    (LogMessage _ time _) -> if newtime < time 
+      then Node (insert m left) n right 
+      else Node left n (insert m right)
 
 build :: [LogMessage] -> MessageTree
-build _ = Leaf
+build msgs = foldl (\tree msg -> insert msg tree) Leaf msgs
 
 inOrder :: MessageTree -> [LogMessage]
-inOrder _ = []
+inOrder Leaf                  = []
+inOrder (Node left msg right) = (inOrder left) ++ [msg] ++ (inOrder right)
+
+filterErrs :: LogMessage -> Bool
+filterErrs (LogMessage (Error sev) _ _) = sev >= 50
+filterErrs _ = False
+
+filterBeforeFailure :: LogMessage -> Bool
+filterBeforeFailure (LogMessage _ time _) = time > -1 && time <= 5395
+filterBeforeFailure _ = False
+
+toMsg :: LogMessage -> String
+toMsg (LogMessage _ _ msg) = msg
+toMsg _ = ""
+
+toMsgFull :: LogMessage -> String
+toMsgFull (LogMessage mtype time msg) = (show mtype) ++ " " ++ (show time) ++ " " ++ msg
+toMsgFull _ = ""
 
 -- whatWentWrong takes an unsorted list of LogMessages, and returns a list of the
 -- messages corresponding to any errors with a severity of 50 or greater,
 -- sorted by timestamp.
 whatWentWrong :: [LogMessage] -> [String]
-whatWentWrong _ = []
+whatWentWrong unsorted = map toMsg errors
+  where sorted = inOrder (build unsorted)
+        errors = filter filterErrs sorted
+
+beforeFailure :: [LogMessage] -> [String]
+beforeFailure unsorted = map toMsgFull errors
+  where sorted = inOrder (build unsorted)
+        errors = filter filterBeforeFailure sorted
+
+allSorted :: [LogMessage] -> [String]
+allSorted unsorted = map toMsg sorted
+  where sorted = inOrder (build unsorted)
+
--- src/Cis194/Hw/Week1.hs
+++ src/Cis194/Hw/Week1.hs
@@ -1,23 +1,36 @@
 module Cis194.Hw.Week1 where
 
+import qualified Data.Map.Strict as Map
+
 -------------
 -- Ex 1-4  --
 -------------
 
 toDigits :: Integer -> [Integer]
-toDigits x = [x]
+toDigits 0 = []
+toDigits x | x < 0 = []
+toDigits x = toDigitsRecur x []
+
+toDigitsRecur :: Integer -> [Integer] -> [Integer]
+toDigitsRecur 0 xs = xs
+toDigitsRecur x xs = toDigitsRecur (x `div` 10) (x `mod` 10 : xs)
 
 toDigitsRev :: Integer -> [Integer]
-toDigitsRev x = [x]
+toDigitsRev x = reverse (toDigits x)
 
 doubleEveryOther :: [Integer] -> [Integer]
-doubleEveryOther xs = xs
+doubleEveryOther [] = []
+doubleEveryOther xs = foldr doubleIfListLenOdd [] $ xs
+
+doubleIfListLenOdd :: Integer -> [Integer] -> [Integer]
+doubleIfListLenOdd x xs | even $ length xs = x : xs
+doubleIfListLenOdd x xs = (x*2) : xs
 
 sumDigits :: [Integer] -> Integer
-sumDigits _ = 0
+sumDigits xs = foldr (\x acc -> acc + (sum $ toDigits x)) 0 xs
 
 validate :: Integer -> Bool
-validate _ = False
+validate x = (sumDigits $ (doubleEveryOther $ toDigits x)) `mod` 10 == 0
 
 ---------------------
 -- Towers of Hanoi --
@@ -27,7 +40,73 @@ type Peg = String
 type Move = (Peg, Peg)
 
 hanoi :: Integer -> Peg -> Peg -> Peg -> [Move]
-hanoi _ _ _ _ = []
+hanoi discs p1 p2 p3 = hanoiRecur discs p1 p2 p3 []
 
-hanoi4 :: Integer -> Peg -> Peg -> Peg -> Peg -> [Move]
-hanoi4 _ _ _ _ _ = []
+hanoiRecur :: Integer -> Peg -> Peg -> Peg -> [Move] -> [Move]
+hanoiRecur discs p1 p2 p3 moves = case discs of
+  0 -> moves
+  1 -> (p1, p2) : moves
+  _ -> (hanoi (discs-1) p1 p3 p2) ++ [(p1,p2)] ++ (hanoi (discs-1) p3 p2 p1)
+
+leastMoves :: [Move] -> [[Move]] -> [Move]
+leastMoves  shortest []    = shortest
+leastMoves [] (x:xs)       = leastMoves x xs
+leastMoves shortest (x:xs) = if length x < length shortest then leastMoves x xs else leastMoves shortest xs
+
+-- I couldn't figure this one out.. had to adapt from:
+-- http://stackoverflow.com/questions/3607161/towers-of-hanoi-with-k-pegs
+hanoiFrameStewart :: Integer -> [Peg] -> [Move]
+hanoiFrameStewart _ []                = []
+hanoiFrameStewart 0 _                 = []
+hanoiFrameStewart _ (p1:(p2:[]))      = [(p1,p2)]
+hanoiFrameStewart n (p1:(p2:(p3:xs))) = 
+  hanoiFrameStewart k ([p1,p3,p2] ++ xs) ++
+  hanoiFrameStewart (n-k) ([p1,p2] ++ xs) ++
+  hanoiFrameStewart k ([p3,p2,p1] ++ xs)
+  where k = if (length xs) == 0 then n - 1 else n `div` 2 
+
+---------------------
+-- Hanoi Validator --
+---------------------
+
+type HanoiState = Map.Map Peg [Integer]
+
+initHanoiState :: Integer -> [Peg] -> HanoiState
+initHanoiState discs pegs = case length pegs of
+  0 -> Map.empty
+  1 -> Map.fromList [((head pegs), [1..discs])]
+  _ -> Map.fromList $ [((head pegs), [1..discs])] ++ map (\peg -> (peg, [])) (tail pegs)
+
+hanoiStateVal :: Peg -> HanoiState -> ([Integer], Bool)
+hanoiStateVal peg state = case Map.lookup peg state of
+  Nothing -> ([], False)
+  Just xs -> (xs, True)
+
+hanoiApplyMove :: Move -> HanoiState -> (HanoiState, Bool)
+hanoiApplyMove move state 
+  | (not fromOk) || (not toOk) = (state, False)    -- one of the pegs in the move doesn't exist
+  | length fromPeg  == 0       = (state, False)    -- there's no disc on the from peg
+  | length newToPeg == 1       = (newState, True)  -- to peg was empty, so moving any disc is ok
+  | fromVal < (head toPeg)     = (newState, True)  -- disc is smaller than top disc on to peg, which is ok
+  | otherwise                  = (state, False)    -- everything else is invalid
+  where (from, to)         = move 
+        (fromPeg, fromOk)  = hanoiStateVal from state
+        (toPeg, toOk)      = hanoiStateVal to   state
+        fromVal            = if length fromPeg > 0 then head fromPeg else 0
+        newToPeg           = fromVal : toPeg
+        newState           = Map.insert to newToPeg (Map.insert from (tail fromPeg) state)
+
+hanoiApplyMoves :: [Move] -> HanoiState -> (HanoiState, Bool)
+hanoiApplyMoves [] state = (state, True)
+hanoiApplyMoves (x:xs) state = case hanoiApplyMove x state of
+  (_, False) -> (state, False)
+  (newState, True) -> hanoiApplyMoves xs newState
+
+hanoiMovesValid :: Integer -> [Peg] -> [Move] -> Bool
+hanoiMovesValid discs pegs moves = 
+  let initial = initHanoiState discs pegs
+  in case hanoiApplyMoves moves initial of
+    (_, False) -> False
+    -- check if result's 2nd peg matches the initial state of the 1st peg
+    (result, True) -> (hanoiStateVal (head pegs) initial) == (hanoiStateVal (pegs !! 1) result)
+
