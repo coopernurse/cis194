ee47fd1e4b20ed01cbcc3bf9a010b076afba6454
James Cooper <jamespcooper@gmail.com>
2014-03-18 23:59:20 -0400

    Implementation of weeks 1-4

--- src/Cis194/Hw/Golf.hs
+++ src/Cis194/Hw/Golf.hs
@@ -1,10 +1,28 @@
 module Cis194.Hw.Golf where
 
+skipsOne :: Int -> [a] -> [a]
+skipsOne _ [] = []
+skipsOne n (x:xs) = [x] ++ skipsOne n (drop n xs)
+
 skips :: [a] -> [[a]]
-skips _ = []
+skips [] = []
+skips xs = map (\n -> skipsOne n $ drop n xs) [0..((length xs) - 1)]
 
 localMaxima :: [Integer] -> [Integer]
+localMaxima (x:y:z:xs) = if y > x && y > z then y : (localMaxima (y:(z:xs))) else localMaxima (y:(z:xs))
 localMaxima _ = []
 
+histLineStr :: [Bool] -> String
+histLineStr xs = map (\b -> if b then '*' else ' ') xs
+
+freq :: Integer -> [Integer] -> Int
+freq x xs = length $ filter (\y -> x == y) xs
+
+toHistLine :: Int -> [Int] -> [Bool]
+toHistLine n freqs = map (\f -> f >= n) freqs
+
 histogram :: [Integer] -> String
-histogram _ = ""
+histogram xs = (unlines (map histLineStr hlines)) ++ "==========\n0123456789\n"
+  where fr = map (\n -> freq n xs) [0..9]
+        maxFreq = maximum fr
+        hlines = map (\x -> toHistLine x fr) (reverse [1..maxFreq])
--- src/Cis194/Hw/Week4.hs
+++ src/Cis194/Hw/Week4.hs
@@ -7,7 +7,7 @@ fun1 (x:xs)
   | otherwise = fun1 xs
 
 fun1' :: [Integer] -> Integer
-fun1' _ = 0
+fun1' = product . map (\x -> x - 2) . filter even
 
 fun2 :: Integer -> Integer
 fun2 1 = 0
@@ -15,18 +15,38 @@ fun2 n | even n = n + fun2 (n `div` 2)
   | otherwise = fun2 (3 * n + 1)
 
 fun2' :: Integer -> Integer
-fun2' _ = 0
+fun2' = sum . filter even . takeWhile (>1) . iterate next 
+  where next x = if even x then x `div` 2 else 3 * x + 1
 
 data Tree a = Leaf
   | Node Integer (Tree a) a (Tree a)
   deriving (Show, Eq)
 
+nodeHeight :: Tree a -> Integer
+nodeHeight Leaf = -1
+nodeHeight (Node _ l _ r) = maximum [(nodeHeight l), (nodeHeight r)] + 1
+
+nodeCount :: Tree a -> Integer
+nodeCount Leaf = 0
+nodeCount (Node _ l _ r) = (nodeCount l) + (nodeCount r) + 1
+
+insertTree :: a -> Tree a -> Tree a
+insertTree x Leaf = Node 0 Leaf x Leaf
+insertTree x (Node _ l n r) = newMe
+  where leftH  = nodeCount l 
+        rightH = nodeCount r 
+        child  = if leftH <= rightH then (insertTree x l) 
+                 else (insertTree x r)
+        newH   = (nodeHeight child) + 1
+        newMe  = if leftH <= rightH then Node newH child n r
+                 else Node newH l n child
+
 foldTree :: [a] -> Tree a
-foldTree _ = Leaf
+foldTree = foldr insertTree Leaf
 
 xor :: [Bool] -> Bool
-xor _ = False
+xor = foldr (/=) False
 
 map' :: (a -> b) -> [a] -> [b]
-map' _ _ = []
+map' f xs = foldr (\x acc -> f x : acc) [] xs 
 
