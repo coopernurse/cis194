c387af95016b1932545a15acd953f0a530085c47
Erin Swenson-Healey <erin.swenson.healey@gmail.com>
2014-03-18 22:44:13 -0700

    completion of exercises 1-4

--- src/Cis194/Hw/Week1.hs
+++ src/Cis194/Hw/Week1.hs
@@ -6,22 +6,25 @@ module Cis194.Hw.Week1 where
 
 toDigits :: Integer -> [Integer]
 toDigits x
-  | x < 1 = []
+  | x < 1  = []
+  | x < 10 = [x]
   | otherwise = toDigits(x `div` 10) ++ [(x `mod` 10)]
 
 toDigitsRev :: Integer -> [Integer]
-toDigitsRev x
-  | x < 1 = []
-  | otherwise = (x `mod` 10) : toDigitsRev(x `div` 10)
+toDigitsRev x = reverse(toDigits(x))
 
 doubleEveryOther :: [Integer] -> [Integer]
-doubleEveryOther xs = xs
+doubleEveryOther [] = []
+doubleEveryOther x  = reverse (map (\(digit, offset) -> if (offset `mod` 2) == 1 then digit * 2 else digit) (zip (reverse x) [0..]))
 
 sumDigits :: [Integer] -> Integer
-sumDigits _ = 0
+sumDigits [] = 0
+sumDigits (x:xs)
+  | x < 10    = x + sumDigits(xs)
+  | otherwise = sumDigits(toDigits(x)) + sumDigits(xs)
 
 validate :: Integer -> Bool
-validate _ = False
+validate x = (sumDigits (doubleEveryOther (toDigits x)) `mod` 10) == 0
 
 ---------------------
 -- Towers of Hanoi --
