9b05251778b1ed991ec3a2325232441d028fd635
Alex Cruikshank <alex@carbonfive.com>
2014-03-26 13:00:50 -0400

    porting week1 homework over to this project

--- src/Cis194/Hw/Week1.hs
+++ src/Cis194/Hw/Week1.hs
@@ -5,19 +5,27 @@ module Cis194.Hw.Week1 where
 -------------
 
 toDigits :: Integer -> [Integer]
-toDigits x = [x]
+toDigits n = reverse . toDigitsRev $ n
 
 toDigitsRev :: Integer -> [Integer]
-toDigitsRev x = [x]
+toDigitsRev n
+  | n < 1 = []
+  | otherwise = (n `mod` 10) : toDigitsRev (n `div` 10)
+
+doubleEveryOtherWithParity :: [Integer] -> (Bool, [Integer])
+doubleEveryOtherWithParity [] = (False, [])
+doubleEveryOtherWithParity (x:xs) = case (doubleEveryOtherWithParity xs) of
+  (True, ys) -> (False, (2*x) : ys)
+  (False, ys) -> (True, x : ys)
 
 doubleEveryOther :: [Integer] -> [Integer]
-doubleEveryOther xs = xs
+doubleEveryOther xs = snd (doubleEveryOtherWithParity xs)
 
 sumDigits :: [Integer] -> Integer
-sumDigits _ = 0
+sumDigits = (foldr (+) 0) . (>>= toDigitsRev)
 
 validate :: Integer -> Bool
-validate _ = False
+validate n = 0 == ((sumDigits (doubleEveryOther (toDigits n) >>= toDigitsRev)) `mod` 10)
 
 ---------------------
 -- Towers of Hanoi --
@@ -27,7 +35,17 @@ type Peg = String
 type Move = (Peg, Peg)
 
 hanoi :: Integer -> Peg -> Peg -> Peg -> [Move]
-hanoi _ _ _ _ = []
+hanoi 0 _ _ _ = []
+hanoi n a b c = (hanoi (n-1) a c b) ++ [(a,b)] ++ (hanoi (n-1) c b a)
+
 
 hanoi4 :: Integer -> Peg -> Peg -> Peg -> Peg -> [Move]
-hanoi4 _ _ _ _ _ = []
+hanoi4 0 _ _ _ _ = []
+hanoi4 n a b c d = (hanoi4 (n-threePeg) a d b c) ++ (hanoi threePeg a b c) ++ (hanoi4 (n-threePeg) d b a c)
+  where threePeg = optimalThreePeg n
+
+quadratic :: (Floating a) => a -> a -> a -> a
+quadratic a b c = ((-b) + sqrt(b*b - 4*a*c)) / (2*a)
+
+optimalThreePeg :: (Integral a) => a -> a
+optimalThreePeg = floor . (quadratic 1 1) . fromIntegral . negate . (*2)
