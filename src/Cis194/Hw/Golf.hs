1c1e3e64f5482c5af36ff42393df104f6689b9d0
Erin Swenson-Healey <erin.swenson.healey@gmail.com>
2014-04-01 23:51:39 -0700

    week 3: implementing 'localMaxima' in terms of 'chunk'

--- src/Cis194/Hw/Golf.hs
+++ src/Cis194/Hw/Golf.hs
@@ -1,5 +1,7 @@
 module Cis194.Hw.Golf where
 
+import Data.Maybe
+
 -- generate all the skips
 -- using map and partial function application
 skips :: [a] -> [[a]]
@@ -13,8 +15,19 @@ skip l n = case drop n l of
   []   -> []
   x:xs -> x : skip xs n
 
+-- find local maxima in a list
+-- by chunking into sub-lists of length 3
+-- and then comparing items in sub-lists
 localMaxima :: [Integer] -> [Integer]
-localMaxima _ = []
+localMaxima xs = mapMaybe match (chunk 3 xs)
+  where match (x:y:z:_) = if y > x && y > z then Just y else Nothing
+        match _ = Nothing
+
+-- chunk a list into sub-lists of length n
+-- e.g. chunk 2 [1, 2, 3] -> [[1, 2], [2, 3]]
+chunk :: Int -> [a] -> [[a]]
+chunk n (x:l@(y:z:_)) = [x, y, z] : chunk n l
+chunk _ _ = []
 
 histogram :: [Integer] -> String
 histogram _ = ""
