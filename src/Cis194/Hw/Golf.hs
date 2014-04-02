d12fc9d072554d12fc9ac8260b0c732b50baff4c
Erin Swenson-Healey <erin.swenson.healey@gmail.com>
2014-04-01 22:19:21 -0700

    week 3: implementing 'skips' in terms of 'skip'

--- src/Cis194/Hw/Golf.hs
+++ src/Cis194/Hw/Golf.hs
@@ -1,7 +1,17 @@
 module Cis194.Hw.Golf where
 
+-- generate all the skips
+-- using map and partial function application
 skips :: [a] -> [[a]]
-skips _ = []
+skips []  = []
+skips x@(_:_) = map (skip x) [0..length x - 1]
+
+-- skip every n items in a list
+-- using drop, recursively
+skip :: [a] -> Int -> [a]
+skip l n = case drop n l of
+  []   -> []
+  x:xs -> x : skip xs n
 
 localMaxima :: [Integer] -> [Integer]
 localMaxima _ = []
