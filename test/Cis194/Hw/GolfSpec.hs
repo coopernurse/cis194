c0579ffec15d7be4d2795c9669ca550d64616a5c
Erin Swenson-Healey <erin.swenson.healey@gmail.com>
2014-04-01 22:54:35 -0700

    added tests for localMaxima and histogram

--- test/Cis194/Hw/GolfSpec.hs
+++ test/Cis194/Hw/GolfSpec.hs
@@ -21,4 +21,30 @@ spec = do
 
     it "should handle an empty list" $ do
       -- not sure if we can avoid a type annotation here
-      skips ([] :: [Integer]) `shouldBe` [] 
+      skips ([] :: [Integer]) `shouldBe` []
+
+  describe "Local Maxima" $ do
+    it "should handle an empty list" $ do
+      localMaxima ([] :: [Integer]) `shouldBe` []
+
+    it "should return an empty list if given a list of length 2 or 1" $ do
+      localMaxima [1, 2] `shouldBe` []
+      localMaxima [2] `shouldBe` []
+
+    it "should return an empty list if no maxima exists" $ do
+      localMaxima [1, 2, 3] `shouldBe` []
+
+    it "should return a maxima" $ do
+      localMaxima [1, 4, 2] `shouldBe` [4]
+
+    it "should handle multiple maximas" $ do
+      localMaxima [1, 4, 2, 9, 3] `shouldBe` [4, 9]
+
+  describe "Histogram" $ do
+    it "should handle an empty list" $ do
+      histogram [] `shouldBe` "==========\n0123456789\n"
+
+    it "should render asterisks representing the freq of each integer" $ do
+      histogram [1, 2, 3] `shouldBe` " ***      \n==========\n0123456789\n"
+      histogram [1, 2, 1] `shouldBe` " *        \n **       \n==========\n0123456789\n"
+
