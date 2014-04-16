88dc8af51273843815e101d01f3a1d8fc0a93876
Alex Cruikshank <alex@carbonfive.com>
2014-04-16 15:23:24 -0400

    Ex 1 & 2 of Homework 5

--- src/Cis194/Hw/Calc.hs
+++ src/Cis194/Hw/Calc.hs
@@ -1,9 +1,14 @@
 module Cis194.Hw.Calc where 
 
-import  Cis194.Hw.ExprT
+import Cis194.Hw.ExprT
+import Cis194.Hw.Parser
 
 eval :: ExprT -> Integer
-eval _ = 0
+eval (Lit x) = x
+eval (Add e1 e2) = (eval e1) + (eval e2)
+eval (Mul e1 e2) = (eval e1) * (eval e2)
 
 evalStr :: String -> Maybe Integer
-evalStr _ = Nothing
+evalStr s = case (parseExp Lit Add Mul s) of
+  Nothing -> Nothing
+  Just expr -> Just (eval expr)
