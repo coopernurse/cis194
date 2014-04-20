5d951647cfafd81cedd6153aa1171a5ff02d7880
Erin Swenson-Healey <erin.swenson.healey@gmail.com>
2014-04-20 14:48:54 -0700

    week 5: evalStr implementation

--- src/Cis194/Hw/Calc.hs
+++ src/Cis194/Hw/Calc.hs
@@ -1,12 +1,22 @@
 module Cis194.Hw.Calc where
 
-import  Cis194.Hw.ExprT
+import Cis194.Hw.ExprT
+import Cis194.Hw.Parser
 
+-- pass an expression and get back an integer
+-- e.g. Lit 4 == 4
+--      Add (Lit 1) (Lit 4) == 5
 eval :: ExprT -> Integer
 eval e1 = case e1 of
   (Lit n) -> n
   (Add e2 e3) -> eval e2 + eval e3
   (Mul e2 e3) -> eval e2 * eval e3
 
+-- pass a string that can be parsed into an
+-- expression and maybe get an integer from its
+-- evaluation
+-- e.g. evalStr "1+5*2" == 12
 evalStr :: String -> Maybe Integer
-evalStr _ = Nothing
+evalStr s = case (parseExp Lit Add Mul s) of
+  Nothing -> Nothing
+  (Just e) -> Just (eval e)
