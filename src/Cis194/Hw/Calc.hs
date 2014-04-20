7e981c4f62c594904bc19731dc94eeadde96f664
Erin Swenson-Healey <erin.swenson.healey@gmail.com>
2014-04-20 14:38:06 -0700

    week 5: eval implementation

--- src/Cis194/Hw/Calc.hs
+++ src/Cis194/Hw/Calc.hs
@@ -1,9 +1,12 @@
-module Cis194.Hw.Calc where 
+module Cis194.Hw.Calc where
 
 import  Cis194.Hw.ExprT
 
 eval :: ExprT -> Integer
-eval _ = 0
+eval e1 = case e1 of
+  (Lit n) -> n
+  (Add e2 e3) -> eval e2 + eval e3
+  (Mul e2 e3) -> eval e2 * eval e3
 
 evalStr :: String -> Maybe Integer
 evalStr _ = Nothing
