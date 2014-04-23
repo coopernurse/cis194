64d910f601eb28e787d267754c549bbc675218c3
Erin Swenson-Healey <erin.swenson.healey@gmail.com>
2014-04-23 08:37:52 -0700

    Adding Week 6 stub

---
+++ src/Cis194/Hw/Fibonacci.hs
@@ -0,0 +1,101 @@
+module Cis194.Hw.Fibonacci where
+
+----------
+-- Ex 1 --
+----------
+
+-- Translate the above definition of Fibonacci numbers directly into a
+-- recursive function definition of type:
+--
+fib :: Integer -> Integer
+fib _ = 0
+--
+-- so that fib n computes the nth Fibonacci number Fn. Then, use fib to
+-- define the infinite list of all Fibonacci numbers:
+--
+-- fibs1 :: [Integer]
+
+fibs1 :: [Integer]
+fibs1 = []
+
+----------
+-- Ex 2 --
+----------
+
+-- Define the infinite list:
+--
+-- fibs2 :: [Integer]
+--
+-- so that it has the same elements as fibs, but computing the first n
+-- elements of fibs2 requires only O(n) addition operations. Be sure to
+-- use standard recursion pattern(s) from Prelude, as appropriate.
+--
+
+
+----------
+-- Ex 3 --
+----------
+
+-- * Define a data type of polymorphic streams, Stream.
+-- * Write a function to convert a Stream to an infinite list:
+--
+--   streamToList :: Stream a -> [a]
+--
+-- * Make your own instance of Show for Stream:
+--
+--   instance Show a => Show (Stream a) where
+--     show ...
+--
+--   ...which works by showing only some prefix of a stream (say, the first
+--   20 elements)
+
+----------
+-- Ex 4 --
+----------
+
+-- * Write a function:
+--
+-- streamRepeat :: a -> Stream a
+--
+-- ...which generates a stream containing infinitely many copies of the
+-- given element
+--
+-- * Write a function:
+--
+-- streamMap :: (a -> b) -> Stream a -> Stream b
+--
+-- ...which applies a function to every element of a Stream
+--
+-- * Write a function:
+--
+-- streamFromSeed :: (a -> a) -> a -> Stream a
+--
+-- ...which generates a Stream from a “seed” of type a, which is the first
+-- element of the stream, and an “unfolding rule” of type a -> a which
+-- specifies how to transform the seed into a new seed, to be used for
+-- generating the rest of the stream.
+
+----------
+-- Ex 5 --
+----------
+
+-- * Define the stream:
+--
+-- nats :: Stream Integer
+--
+-- ...which contains the infinite list of natural numbers 0, 1, 2...
+--
+-- * Define the stream:
+--
+-- ruler :: Stream Integer
+--
+-- ...which corresponds to the ruler function:
+--
+-- 0,1,0,2,0,1,0,3,0,1,0,2,0,1,0,4,...
+--
+-- ...where the nth element in the stream (assuming the first element
+-- corresponds to n = 1) is the largest power of 2 which evenly divides n.
+--
+-- Hint: define a function interleaveStreams which alternates the
+-- elements from two streams. Can you use this function to implement ruler
+-- in a clever way that does not have to do any divisibility testing?
