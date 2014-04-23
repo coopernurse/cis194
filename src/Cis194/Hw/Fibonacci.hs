1dba9c3e193e625eeda4f5a7a80c50b9f8a653fd
Erin Swenson-Healey <erin.swenson.healey@gmail.com>
2014-04-23 16:54:16 -0700

    week 6: streams impls

--- src/Cis194/Hw/Fibonacci.hs
+++ src/Cis194/Hw/Fibonacci.hs
@@ -8,7 +8,9 @@ module Cis194.Hw.Fibonacci where
 -- recursive function definition of type:
 --
 fib :: Integer -> Integer
-fib _ = 0
+fib 0 = 0
+fib 1 = 1
+fib n = fib (n - 1) + fib (n - 2)
 --
 -- so that fib n computes the nth Fibonacci number Fn. Then, use fib to
 -- define the infinite list of all Fibonacci numbers:
@@ -16,7 +18,7 @@ fib _ = 0
 -- fibs1 :: [Integer]
 
 fibs1 :: [Integer]
-fibs1 = []
+fibs1 = map fib [0..]
 
 ----------
 -- Ex 2 --
@@ -26,21 +28,39 @@ fibs1 = []
 --
 -- fibs2 :: [Integer]
 --
--- so that it has the same elements as fibs, but computing the first n
+-- so that it has the same elements as fibs1, but computing the first n
 -- elements of fibs2 requires only O(n) addition operations. Be sure to
 -- use standard recursion pattern(s) from Prelude, as appropriate.
 --
 
+-- Since Haskell is lazy, we will only compute items in the fibs2 list
+-- when we need them. Computing the third item in the list equates to
+-- cons'ing 0 to 1 to the result of a list comprehension. The list
+-- comprehension creates tuples out of each item in the CURRENT fibs list
+-- list with the item immediately before it, e.g.:
+--
+-- let x = [1, 2, 3]
+-- zip x (tail x) == [(1,2), (2, 3)]
+--
+-- The sum of the two items in the tuple gets us our fib sequence.
+fibs2 :: [Integer]
+fibs2 = 0 : 1 : [a + b | (a, b) <- zip fibs2 (tail fibs2)]
 
 ----------
 -- Ex 3 --
 ----------
 
 -- * Define a data type of polymorphic streams, Stream.
+
+data Stream a = Cons a (Stream a)
+
 -- * Write a function to convert a Stream to an infinite list:
 --
 --   streamToList :: Stream a -> [a]
---
+
+streamToList :: Stream a -> [a]
+streamToList (Cons x xs) = x : streamToList xs
+
 -- * Make your own instance of Show for Stream:
 --
 --   instance Show a => Show (Stream a) where
@@ -48,6 +68,9 @@ fibs1 = []
 --
 --   ...which works by showing only some prefix of a stream (say, the first
 --   20 elements)
+
+instance Show a => Show (Stream a) where
+  show s = unwords . take 20 $ map show $ streamToList s
 
 ----------
 -- Ex 4 --
@@ -59,13 +82,19 @@ fibs1 = []
 --
 -- ...which generates a stream containing infinitely many copies of the
 -- given element
---
+
+streamRepeat :: a -> Stream a
+streamRepeat e = Cons e $ streamRepeat e
+
 -- * Write a function:
 --
 -- streamMap :: (a -> b) -> Stream a -> Stream b
 --
 -- ...which applies a function to every element of a Stream
---
+
+streamMap :: (a -> b) -> Stream a -> Stream b
+streamMap f (Cons x xs) = Cons (f x) $ streamMap f xs
+
 -- * Write a function:
 --
 -- streamFromSeed :: (a -> a) -> a -> Stream a
@@ -74,6 +103,9 @@ fibs1 = []
 -- element of the stream, and an “unfolding rule” of type a -> a which
 -- specifies how to transform the seed into a new seed, to be used for
 -- generating the rest of the stream.
+
+streamFromSeed :: (a -> a) -> a -> Stream a
+streamFromSeed f seed = Cons seed $ streamFromSeed f $ f seed
 
 ----------
 -- Ex 5 --
@@ -84,7 +116,10 @@ fibs1 = []
 -- nats :: Stream Integer
 --
 -- ...which contains the infinite list of natural numbers 0, 1, 2...
---
+
+nats :: Stream Integer
+nats = streamFromSeed (+1) 0
+
 -- * Define the stream:
 --
 -- ruler :: Stream Integer
@@ -99,3 +134,6 @@ fibs1 = []
 -- Hint: define a function interleaveStreams which alternates the
 -- elements from two streams. Can you use this function to implement ruler
 -- in a clever way that does not have to do any divisibility testing?
+
+interleaveStreams :: Stream a -> Stream a -> Stream a
+interleaveStreams (Cons x xs) ys = Cons x $ interleaveStreams ys xs
