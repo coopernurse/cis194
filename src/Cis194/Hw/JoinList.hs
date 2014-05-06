cba91ba32c8dd0cbd2ed6f5115245cb8864812e8
Erin Swenson-Healey <erin.swenson.healey@gmail.com>
2014-05-05 21:55:04 -0700

    week 7 helper functions

--- src/Cis194/Hw/JoinList.hs
+++ src/Cis194/Hw/JoinList.hs
@@ -27,6 +27,21 @@ tag (Append m jl1 jl2) = tag jl1 `mappend` tag jl2
 
 
 -- ** Exercise 2
+--
+-- Helper functions:
+--
+
+(!!?) :: [a] -> Int -> Maybe a
+[]     !!? _         = Nothing
+_      !!? i | i < 0 = Nothing
+(x:xs) !!? 0 = Just x
+(x:xs) !!? i = xs !!? (i-1)
+
+jlToList :: JoinList m a -> [a]
+jlToList Empty            = []
+jlToList (Single _ a)     = [a]
+jlToList (Append _ l1 l2) = jlToList l1 ++ jlToList l2
+
 -- 1. Implement the function:
 --    indexJ :: (Sized b, Monoid b) => Int -> JoinList b a -> Maybe a
 --    ...which finds the JoinList element at the specified index
@@ -40,3 +55,11 @@ indexJ n (Append m jl1 jl2)
   | n < leftSize = indexJ n jl1
   | otherwise = indexJ (n - leftSize) jl2
   where leftSize = getSize . size $ (tag jl1)
+
+
+-- 2. Implement the function:
+-- dropJ :: (Sized b, Monoid b) => Int -> JoinList b a -> JoinList b a
+-- ...which drops the first n elements from a JoinList.
+
+dropJ :: (Sized b, Monoid b) => Int -> JoinList b a -> JoinList b a
+dropJ _ (Empty) = Empty
