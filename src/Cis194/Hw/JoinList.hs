9305a07f706fb7c32d0a1a79873ca3d570c9d129
Erin Swenson-Healey <erin.swenson.healey@gmail.com>
2014-05-05 21:49:44 -0700

    week 7: indexJ impl

--- src/Cis194/Hw/JoinList.hs
+++ src/Cis194/Hw/JoinList.hs
@@ -1,5 +1,9 @@
 import Data.Monoid
 
+-- in ghci, you may need to specify an additional include path:
+-- Prelude> :set -isrc/Cis194/Hw
+import Sized
+
 data JoinList m a = Empty
                    | Single m a
                    | Append m (JoinList m a) (JoinList m a)
@@ -19,3 +23,20 @@ tag :: Monoid m => JoinList m a -> m
 tag (Single m a) = m
 tag (Empty) = mempty
 tag (Append m jl1 jl2) = tag jl1 `mappend` tag jl2
+
+
+
+-- ** Exercise 2
+-- 1. Implement the function:
+--    indexJ :: (Sized b, Monoid b) => Int -> JoinList b a -> Maybe a
+--    ...which finds the JoinList element at the specified index
+
+indexJ :: (Sized b, Monoid b) => Int -> JoinList b a -> Maybe a
+indexJ _ (Empty)              = Nothing
+indexJ 0 (Single m a)         = Just a
+indexJ n (Single m a) | n > 0 = Nothing
+indexJ n _            | n < 0 = Nothing
+indexJ n (Append m jl1 jl2)
+  | n < leftSize = indexJ n jl1
+  | otherwise = indexJ (n - leftSize) jl2
+  where leftSize = getSize . size $ (tag jl1)
