ae1d77a6917d4dd8c399d56d9d3d8997fec37125
Erin Swenson-Healey <erin.swenson.healey@gmail.com>
2014-05-05 21:55:04 -0700

    week 7 helper functions

--- src/Cis194/Hw/JoinList.hs
+++ src/Cis194/Hw/JoinList.hs
@@ -51,6 +51,21 @@ jlToList (Single _ a)     = [a]
 jlToList (Append _ l1 l2) = jlToList l1 ++ jlToList l2
 
 -- ** Exercise 2
+--
+-- Helper functions:
+--
+
+(!!?) :: [a] -> Int -> Maybe a
+[]     !!? _         = Nothing
+_      !!? i | i < 0 = Nothing
+(x:xs) !!? 0 = Just x
+(x:xs) !!? i = xs !!? (i-1)
+
+jlToList :: JoinList m a -> [a]
+jlToList Empty            = []
+jlToList (Single _ a)     = [a]
+jlToList (Append _ l1 l2) = jlToList l1 ++ jlToList l2
+
 -- 1. Implement the function:
 --    indexJ :: (Sized b, Monoid b) => Int -> JoinList b a -> Maybe a
 --    ...which finds the JoinList element at the specified index
