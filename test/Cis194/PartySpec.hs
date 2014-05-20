f06a6826c8fc3632e858c17703487f0aee5a405f
Erin Swenson-Healey <erin.swenson.healey@gmail.com>
2014-05-20 08:50:31 -0700

    GuestList Monoid-instance and moreFun tests

--- test/Cis194/PartySpec.hs
+++ test/Cis194/PartySpec.hs
@@ -3,6 +3,7 @@ module Cis194.Hw.PartySpec (main, spec) where
 import Test.Hspec
 import Cis194.Hw.Party
 import Employee
+import Data.Monoid
 
 main :: IO ()
 main = hspec spec
@@ -20,3 +21,25 @@ spec = do
 
       glCons bob empty `shouldBe` GL [bob] 10
       glCons joe notEmpty `shouldBe` GL [joe, bob, sue] 70
+
+  describe "GuestList" $ do
+    it "behaves like a Monoid" $ do
+      let joe = Emp { empFun = 40, empName = "Joe" }
+      let sue = Emp { empFun = 20, empName = "Suzie" }
+
+      let empty = GL [] 0
+      let joeList = GL [joe] 40
+      let sueList = GL [sue] 20
+
+      mappend empty joeList `shouldBe` joeList
+      mappend joeList sueList `shouldBe` GL [joe, sue] 60
+      mconcat [empty, joeList, sueList] `shouldBe` GL [joe, sue] 60
+
+  describe "moreFun" $ do
+    it "returns the GuestList with the highest fun score" $ do
+      let empty = GL [] 0
+      let joeList = GL [Emp { empName = "Joe", empFun = 40 }] 40
+      let sueList = GL [Emp { empName = "Sue", empFun = 20 }] 20
+
+      moreFun empty joeList `shouldBe` joeList
+      moreFun joeList sueList `shouldBe` joeList
