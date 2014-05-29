6d923ac0c5a72f3167b0297686b0580950a9fb6f
James Cooper <jamespcooper@gmail.com>
2014-05-28 20:36:16 -0700

    Week 10 - stub exercises

--- src/Cis194/Hw/AParser.hs
+++ src/Cis194/Hw/AParser.hs
@@ -52,3 +52,62 @@ posInt = Parser f
 ------------------------------------------------------------
 -- Your code goes below here
 ------------------------------------------------------------
+
+-- Ex. 1 - implement a Functor instance for Parser
+--
+-- You may find it useful to implement:
+-- first :: (a -> b) -> (a,c) -> (b,c)
+
+
+-- Ex. 2 - implement an Applicative instance for Parser
+--
+--  pure a represents the parser which consumes no input and successfully returns a result of a.
+--  p1 <*> p2 represents the parser which ﬁrst runs p1 (which will consume some input and 
+-- produce a function), then passes the remaining input to p2 (which consumes more input 
+-- and produces some value), then returns the result of applying the function to the
+-- value. However, if either p1 or p2 fails then the whole thing should also fail (put another
+-- way, p1 <*> p2 only succeeds if both p1 and p2 succeed).
+
+
+-- Ex. 3a - Create a parser:
+--
+--   abParser :: Parser (Char, Char)
+--
+-- which expects to see the characters ’a’ and ’b’ and returns them as a pair
+
+-- Ex. 3b - Create a parser:
+--
+--   abParser_ :: Parser ()
+--
+-- which acts in the same way as abParser but returns () instead of 'a' and 'b'
+
+-- Ex. 3c - Create a parser:
+--
+--   intPair 
+--
+-- which reads two integer values separated by a space and returns the integer 
+-- values in a list. You should use the provided posInt to parse the integer values.
+
+
+-- Ex. 4 - Write an Alternative instance for Parser
+--
+-- See: http://hackage.haskell.org/package/base-4.7.0.0/docs/Control-Applicative.html#t:Alternative
+--
+-- empty represents the parser which always fails.
+-- p1 <|> p2 represents the parser which ﬁrst tries running p1. If p1 succeeds then p2 is
+-- ignored and the result of p1 is returned.  Otherwise, if p1 fails, then p2 is tried instead.
+--
+-- Hint: there is already an Alternative instance for Maybe which you may find useful.
+
+
+-- Ex. 5 - Implement a parser:
+--
+--  intOrUppercase :: Parser ()
+-- 
+-- which parses either an integer value or an uppercase character, and fails otherwise.
+
+
+
+
+
+
