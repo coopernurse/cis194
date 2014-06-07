7c785b907804f24c16c1d690efe2eca5a868bd97
James Cooper <jamespcooper@gmail.com>
2014-06-07 11:26:15 -0700

    week 12: stub functions

--- src/Cis194/Hw/Risk.hs
+++ src/Cis194/Hw/Risk.hs
@@ -26,3 +26,55 @@ die = getRandom
 type Army = Int
 
 data Battlefield = Battlefield { attackers :: Army, defenders :: Army }
+
+-----------------------------
+-- Ex 2. 
+-----------------------------
+
+-- simulates a single battle between two opposing armies. That is, 
+-- it should simulate randomly rolling the appropriate number of dice, 
+-- interpreting the results, and updating the two armies to reﬂect 
+-- casualties. You may assume that each player will attack or defend 
+-- with the maximum number of units they are allowed
+--
+battle :: Battlefield -> Rand StdGen Battlefield
+battle = undefined
+
+-----------------------------
+-- Ex 3. 
+-----------------------------
+
+-- simulates an entire invasion attempt, that is, repeated calls
+-- to battle until there are no defenders remaining, or fewer than two
+-- attackers.
+--
+invade :: Battlefield -> Rand StdGen Battlefield
+invade = undefined
+
+-----------------------------
+-- Ex 4. 
+-----------------------------
+
+-- runs invade 1000 times, and uses the results to compute a
+-- Double between 0 and 1 representing the estimated probability that
+-- the attacking army will completely destroy the defending army.
+-- 
+-- For example, if the defending army is destroyed in 300 of the 1000
+-- simulations (but the attacking army is reduced to 1 unit in the other
+-- 700), successProb should return 0.3.
+--
+successProb :: Battlefield -> Rand StdGen Double
+successProb = undefined
+
+-----------------------------
+-- Ex 5. (Optional)
+-----------------------------
+
+-- computes the exact probability of success based on principles
+-- of probability, without running any simulations. (This won’t give you
+-- any particular practice with Haskell; it’s just a potentially interesting
+-- challenge in probability theory.)
+--
+exactSuccessProb :: Battlefield -> Double
+exactSuccessProb = undefined
+
