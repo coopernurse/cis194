ed60abcf2e2c158c41bb6203ed893ba7af29f520
Erin Swenson-Healey <erin.swenson.healey@gmail.com>
2014-06-12 18:27:13 -0700

    week 3: golfifying localMaxima

--- src/Cis194/Hw/Golf.hs
+++ src/Cis194/Hw/Golf.hs
@@ -13,21 +13,19 @@ skips x@(_:_) = map (skip x) [0..length x - 1]
                      x:xs -> x : skip xs n
 
 -- find local maxima in a list
--- by chunking into sub-lists of length 3
--- and then comparing items in sub-lists
 localMaxima :: [Integer] -> [Integer]
-localMaxima xs = mapMaybe match (chunk xs)
-  where match (x:y:z:_) = if y > x && y > z then Just y else Nothing
-        match _ = Nothing
-
--- chunk a list into sub-lists of length n
--- e.g. chunk 2 [1, 2, 3] -> [[1, 2], [2, 3]]
-chunk :: [a] -> [[a]]
-chunk (x:l@(y:z:_)) = [x, y, z] : chunk l
-chunk _ = []
+localMaxima l = mapMaybe id $ comp l
+  where
+    comp (x:r@(y:z:_)) = (if y > x && y > z then Just y else Nothing) : comp r
+    comp _ = []
 
 histogram :: [Integer] -> String
 histogram = (++"==========\n0123456789\n") . toRows . digitFreqs
+  where
+    dot n = if n > 0 then '*' else ' '
+    toRows list = case (find (>0) list) of
+      Nothing -> ""
+      Just _  -> toRows (map (+(-1)) list) ++ (map (dot) list ++ "\n")
 
 -- computes frequency for each digit given a list
 -- using sentinel value + recursive 'span' to
@@ -38,14 +36,3 @@ digitFreqs l1 = digitFreqs' 0 $ sort l1
           | n == 10 = []
           | otherwise = case (span (==n) l2) of
           (left, right) -> length left : digitFreqs' (n+1) right
-
--- convert a list of frequencies to a String
--- representing our rows
--- e.g. [0,1,0,0,0,0,0,0,0,0] -> " *        \n"
--- by recursively mapping our row to the mark function
--- and short-circuiting when we have no non-zero freqs
-toRows :: [Int] -> String
-toRows list = case (find (>0) list) of
-  Nothing -> ""
-  Just _  -> toRows (map (+(-1)) list) ++ (map (mark) list ++ "\n")
-  where mark n = if n > 0 then '*' else ' '
