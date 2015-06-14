209fa9ec1d113f56c6307f641a2949cdf418020b
Erin Swenson-Healey <erin.swenson.healey@gmail.com>
2015-06-14 15:06:52 -0700

    Ex1 and Ex2 of Hw05

--- src/Cis194/Hw/Bank/Hw05.hs
+++ src/Cis194/Hw/Bank/Hw05.hs
@@ -4,22 +4,35 @@ module HW05 where
 
 import Data.ByteString.Lazy (ByteString)
 import Data.Map.Strict (Map)
-import System.Environment (getArgs)
+import Data.Bits (xor)
+import Data.Word (Word8)
+{-import System.Environment (getArgs)-}
 
 import qualified Data.ByteString.Lazy as BS
-import qualified Data.Map.Strict as Map
+{-import qualified Data.Map.Strict as Map-}
 
 import Parser
+
+-- Helpers --------------------------------------------
+
+xorByteStringsWithFilter :: (Word8 -> Bool) -> ByteString -> ByteString -> ByteString
+xorByteStringsWithFilter predicate bs1 bs2 = BS.pack $ filter (predicate) $ BS.zipWith (xor) bs1 bs2
 
 -- Exercise 1 -----------------------------------------
 
 getSecret :: FilePath -> FilePath -> IO ByteString
-getSecret = undefined
+getSecret origFileName modFileName = do
+  bsOrig <- BS.readFile origFileName
+  bsMod <- BS.readFile modFileName
+  return $ xorByteStringsWithFilter (/=0) bsOrig bsMod
 
 -- Exercise 2 -----------------------------------------
 
 decryptWithKey :: ByteString -> FilePath -> IO ()
-decryptWithKey = undefined
+decryptWithKey key path = do
+  encrypted <- BS.readFile (path ++ ".enc")
+  print $ BS.pack $ BS.zipWith (xor) encrypted (BS.cycle key)
+  BS.writeFile path $ xorByteStringsWithFilter (const True) encrypted (BS.cycle key)
 
 -- Exercise 3 -----------------------------------------
 
@@ -66,22 +79,28 @@ doEverything dog1 dog2 trans vict fids out = do
       case mids of
         Nothing  -> error "No ids"
         Just ids -> do
-          let flow = getFlow ts       
+          let flow = getFlow ts
           writeJSON out (undoTs flow ids)
           return (getCriminal flow)
 
 main :: IO ()
 main = do
-  args <- getArgs
-  crim <- 
-    case args of
-      dog1:dog2:trans:vict:ids:out:_ ->
-          doEverything dog1 dog2 trans vict ids out
-      _ -> doEverything "dog-original.jpg"
-                        "dog.jpg"
-                        "transactions.json"
-                        "victims.json"
-                        "new-ids.json"
-                        "new-transactions.json"
-  putStrLn crim
+  key <- getSecret "dog-original.jpg" "dog.jpg"
+  print key
+  decryptWithKey key "victims.json"
+
+{-main :: IO ()-}
+{-main = do-}
+  {-args <- getArgs-}
+  {-crim <--}
+    {-case args of-}
+      {-dog1:dog2:trans:vict:ids:out:_ ->-}
+          {-doEverything dog1 dog2 trans vict ids out-}
+      {-_ -> doEverything "dog-original.jpg"-}
+                        {-"dog.jpg"-}
+                        {-"transactions.json"-}
+                        {-"victims.json"-}
+                        {-"new-ids.json"-}
+                        {-"new-transactions.json"-}
+  {-putStrLn crim-}
 
